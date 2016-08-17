888c8a3d12e826d4c09eb5bc83af335ff47fd517
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:54:27 +1200

    Set location of source.

--- mapclientplugins/basicplotstep/__init__.py
+++ mapclientplugins/basicplotstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Basic Plot'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/basicplot/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.basicplotstep import step
